e405f4eed743db9730f532fea0dfdcb69bc69a6d
mohamadgamez <46826596+mohamadgamez@users.noreply.github.com>
2019-03-02 06:09:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,17 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 console.log("Scrpit By Dream");
-var adminprefix = '-'
+
+
 
 client.on("ready", () => {
 let channel =     client.channels.get("551139671682318366")
 setInterval(function() {
 channel.send(`spam-by-mody-spam-by-mody`);
 }, 30)
-})
-if (message.content.startsWith(adminprefix + 'pl')) {
-    client.user.setGame(argresult);
-      message.channel.send(`**✅   ${argresult}**`)
-  } else 
+}) 
 
 client.login(process.env.BOT_TOKEN);
